d864a97b23689cba1dd8c78e2bcce20af79f0dc2
David K. Zhang <dzhang314@gmail.com>
2018-05-15 01:29:51 -0500

    Add logging to task server and workers

--- rktk-refinement-worker.py
+++ rktk-refinement-worker.py
@@ -10,39 +10,46 @@ OUTPUT_DIR = "refined"
 
 WORKER_ID = str(uuid.uuid4()).upper()
 WORKER_DIR = "WORKER-" + WORKER_ID
-print("Initializing RKTK refinement worker with ID {0}.".format(WORKER_ID))
+WORKER_LOG = open("WORKER-" + WORKER_ID + ".log", 'w+')
+
+def log(message):
+    print(message, flush=True)
+    print(message, file=WORKER_LOG, flush=True)
+
+log("Initializing RKTK refinement worker with ID {0}.".format(WORKER_ID))
 os.mkdir(WORKER_DIR)
 
 if os.path.isdir(REQUEST_DIR):
-    print("Posting requests in existing \"" +
-          REQUEST_DIR + "\" directory.")
+    log("Posting requests in existing \"" +
+        REQUEST_DIR + "\" directory.")
 else:
     try:
         os.mkdir(REQUEST_DIR)
-        print("Posting requests in newly-created \"" +
-              REQUEST_DIR + "\" directory.")
+        log("Posting requests in newly-created \"" +
+            REQUEST_DIR + "\" directory.")
     except FileExistsError:
-        print("ERROR: A file named \"" +
-              REQUEST_DIR + "\" already exists.")
+        log("ERROR: A file named \"" +
+            REQUEST_DIR + "\" already exists.")
         sys.exit(1)
 
 if os.path.isdir(OUTPUT_DIR):
-    print("Sending output to existing \"" +
-          OUTPUT_DIR + "\" directory.")
+    log("Sending output to existing \"" +
+        OUTPUT_DIR + "\" directory.")
 else:
     try:
         os.mkdir(OUTPUT_DIR)
-        print("Sending output to newly-created \"" +
-              OUTPUT_DIR + "\" directory.")
+        log("Sending output to newly-created \"" +
+            OUTPUT_DIR + "\" directory.")
     except FileExistsError:
-        print("ERROR: A file named \"" +
-              OUTPUT_DIR + "\" already exists.")
+        log("ERROR: A file named \"" +
+            OUTPUT_DIR + "\" already exists.")
         sys.exit(1)
 
 def work():
     REQUEST_ID = str(uuid.uuid4()).upper()
     REQUEST_NAME = "REQUEST-" + REQUEST_ID
     os.mkdir(os.path.join(REQUEST_DIR, REQUEST_NAME))
+    log("Posted request {0}.".format(REQUEST_NAME))
     served = False
     while True:
         for entry in os.scandir(os.path.join(REQUEST_DIR, REQUEST_NAME)):
@@ -51,17 +58,23 @@ def work():
                       os.path.join(WORKER_DIR, INPUT_FILE))
             served = True
         if served:
+            log("Request {0} has been served.".format(REQUEST_NAME))
             break
     os.rmdir(os.path.join(REQUEST_DIR, REQUEST_NAME))
     if INPUT_FILE == "EXHAUSTED":
+        log("Null request received. Terminating worker.")
         return False
-    subprocess.run(['../bin/rksearch', '53', '0.5', '0', INPUT_FILE],
+    log("Running RKSearch refinement job.")
+    subprocess.run(['../bin/rksearch', '64', '10', '0', INPUT_FILE],
                    cwd=WORKER_DIR, stderr=subprocess.DEVNULL)
+    log("Running cleanup script.")
     subprocess.run(['python3', '../rktk-cleanup.py'],
                    cwd=WORKER_DIR)
+    log("Submitting job results.")
     for entry in os.scandir(WORKER_DIR):
         os.rename(entry.path,
                   os.path.join(OUTPUT_DIR, entry.name))
+    log("Job completed.")
     return True
 
 while True:
@@ -69,3 +82,4 @@ while True:
 
 os.remove(os.path.join(WORKER_DIR, "EXHAUSTED"))
 os.rmdir(WORKER_DIR)
+WORKER_LOG.close()
--- rktk-task-server.py
+++ rktk-task-server.py
@@ -10,15 +10,15 @@ REQUEST_DIR = "requests"
 
 if os.path.isdir(REQUEST_DIR):
     print("Serving requests in existing \"" +
-          REQUEST_DIR + "\" directory.")
+          REQUEST_DIR + "\" directory.", flush=True)
 else:
     try:
         os.mkdir(REQUEST_DIR)
         print("Serving requests in newly-created \"" +
-              REQUEST_DIR + "\" directory.")
+              REQUEST_DIR + "\" directory.", flush=True)
     except FileExistsError:
         print("ERROR: A file named \"" +
-              REQUEST_DIR + "\" already exists.")
+              REQUEST_DIR + "\" already exists.", flush=True)
         sys.exit(1)
 
 served = {}
@@ -33,13 +33,13 @@ while True:
     #     (3) Acknowledged: when its requestor deletes its request directory.
     acknowledged = served.keys() - requests
     for name in acknowledged:
-        print("Task {0} has been acknowledged.".format(name))
+        print("Task {0} has been acknowledged.".format(name), flush=True)
         del served[name]
     present = datetime.datetime.now()
     timedout = set()
     for name, timestamp in served.items():
         if present - timestamp > datetime.timedelta(seconds=10):
-            print("Task {0} has timed out.".format(name))
+            print("Task {0} has timed out.".format(name), flush=True)
             for item in os.listdir(os.path.join(REQUEST_DIR, name)):
                 os.rename(os.path.join(REQUEST_DIR, name, item),
                           os.path.join(TASK_DIR, item))
@@ -51,13 +51,13 @@ while True:
     for name in created:
         served[name] = present
         for item in os.scandir(TASK_DIR):
-            print("Serving request {0} with task {1}.".format(name, item.path))
+            print("Serving request {0} with task {1}.".format(name, item.path), flush=True)
             os.rename(item.path,
                       os.path.join(REQUEST_DIR, name, item.name))
             break
         else:
-            print("Serving request {0} with null task.".format(name, item.path))
+            print("Serving request {0} with null task.".format(name, item.path), flush=True)
             with open(os.path.join(REQUEST_DIR, name, "EXHAUSTED"),
-                        'a') as dummyfile:
+                      'a') as dummyfile:
                 pass
     # time.sleep(1)
