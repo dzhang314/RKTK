1ed4fc13d381814c63662cb88ae6fb5892697df9
David K. Zhang <dzhang314@gmail.com>
2019-01-06 16:59:42 -0600

    Add auxiliary args to golden_section_search

--- scripts/GoldenSectionSearch.jl
+++ scripts/GoldenSectionSearch.jl
@@ -6,12 +6,12 @@ export golden_section_search
 Base.@irrational INV_PHI    0.61803398874989484820  (sqrt(big(5)) - 1) / 2
 Base.@irrational INV_PHI_2  0.38196601125010515180  (3 - sqrt(big(5))) / 2
 
-function golden_section_search(f, a, b, n)
+function golden_section_search(f, a, b, n, args...)
     h = b - a
     c = a + INV_PHI_2 * h
     d = a + INV_PHI * h
-    yc = f(c)
-    yd = f(d)
+    yc = f(c, args...)
+    yd = f(d, args...)
     for i = 1 : n
         if yc < yd
             b = d
@@ -19,14 +19,14 @@ function golden_section_search(f, a, b, n)
             yd = yc
             h *= INV_PHI
             c = a + INV_PHI_2 * h
-            yc = f(c)
+            yc = f(c, args...)
         else
             a = c
             c = d
             yc = yd
             h *= INV_PHI
             d = a + INV_PHI * h
-            yd = f(d)
+            yd = f(d, args...)
         end
     end
     if yc < yd
