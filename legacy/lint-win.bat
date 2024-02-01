aacad4002f8418738632d5094f2b275ff8f9248b
David K. Zhang <dzhang314@gmail.com>
2024-01-31 18:28:44 -0800

    delete obsolete legacy code

--- legacy/lint-win.bat
+++
@@ -1,7 +0,0 @@
-clang++ -std=c++17 ^
--Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic ^
--Wno-shorten-64-to-32 -Wno-sign-conversion -Wno-float-equal -Wno-padded ^
--isystem C:\Programs\boost_1_69_0\ ^
--isystem C:\Programs\eigen-eigen-323c052e1731\ ^
--I C:\Users\Zhang\Documents\GitHub\dznl\ ^
-.\rksearch-main.cpp
