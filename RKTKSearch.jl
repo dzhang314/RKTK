dc7297b32e916eca9999ac3a3e4d891be013d723
David K. Zhang <dzhang314@gmail.com>
2024-02-22 01:38:06 -0800

    Update RKTKSearch.jl

--- RKTKSearch.jl
+++ RKTKSearch.jl
@@ -7,23 +7,42 @@ using Printf
 using RungeKuttaToolKit
 
 
+const USAGE_STRING = """
+Usage: julia [jl_options] $PROGRAM_FILE <order> <num_stages> [seed] [seed]
+[jl_options] refers to Julia options, such as -O3 or --math-mode=fast.
+
+<order> and <num_stages> must be positive integers between 1 and 99.
+
+If no seed is specified, then a search is performed for every possible seed,
+    starting from zero and counting up through every unsigned 64-bit integer.
+If one seed is specified, it is treated as an upper bound (inclusive).
+If two seeds are specified, they are treated as bounds (inclusive).
+"""
+
+
 const WRITE_FILE = !("--no-file" in ARGS)
 const WRITE_TERM = ("--no-file" in ARGS) || (
     (stdout isa Base.TTY) && (nthreads() == 1))
-filter!(arg -> (arg != "--no-file"), ARGS)
+const REQUESTED_EXPLICIT = ("--explicit" in ARGS)
+const REQUESTED_IMPLICIT = ("--implicit" in ARGS)
+const REQUESTED_EXACT_B = ("--exact-b" in ARGS)
+const REQUESTED_APPROXIMATE_B = ("--approximate-b" in ARGS)
+filter!(arg -> (
+        (arg != "--no-file") && (arg != "--explicit") &&
+        (arg != "--implicit") && (arg != "--exact-b") &&
+        (arg != "--approximate-b")), ARGS)
 
 
 function fprintln(io::IO, args...)
-    if WRITE_FILE
+    @static if WRITE_FILE
         println(io, args...)
         flush(io)
-    else
-        @assert io === devnull
     end
-    if WRITE_TERM
+    @static if WRITE_TERM
         println(stdout, args...)
         flush(stdout)
     end
+    return nothing
 end
 
 
@@ -38,16 +57,17 @@ function fprint_status(io::IO, opt::LBFGSOptimizer; force::Bool=false)
             opt.iteration_count[],
             sqrt(opt.current_objective_value[] / num_residuals),
             sqrt(norm2(opt.current_gradient) / num_variables),
-            sqrt(norm2(opt.current_point) / num_variables),
+            sqrt(maximum(abs, opt.current_point)),
             sqrt(norm2(opt.delta_point) / num_variables),
             reset_occurred ? " RESET" : ""))
     end
+    return nothing
 end
 
 
 function find_existing_file(prefix::AbstractString, suffix::AbstractString)
     for filename in readdir()
-        if (length(filename) == 46 &&
+        if (length(filename) == 51 &&
             startswith(filename, prefix) && endswith(filename, suffix))
             return filename
         end
@@ -57,45 +77,61 @@ end
 
 
 function search(
-    evaluator::RKOCEvaluator{Float64},
-    order::Int, num_stages::Int, seed::UInt64
+    evaluator, order::Int, num_stages::Int, mode::String, seed::UInt64
 )
-    tempname = @sprintf("RKTK-%02d-%02d-XXXX-XXXX-XXXX-%016X.txt",
-        order, num_stages, seed)
-    @assert length(tempname) == 46
+    @assert length(mode) == 4
+    filename = @sprintf("RKTK-%02d-%02d-%s-XXXX-XXXX-XXXX-%016X.txt",
+        order, num_stages, mode, seed)
+    @assert length(filename) == 51
+
     if WRITE_FILE
-        existing = find_existing_file(tempname[1:11], tempname[26:46])
+        existing = find_existing_file(filename[1:16], filename[31:51])
         if isnothing(existing)
-            println("Computing $tempname...")
+            println("Computing $filename...")
         else
             println("$existing already exists.")
             return nothing
         end
     end
 
-    num_variables = ((num_stages + 1) * num_stages) >> 1
+    num_variables =
+        (mode == "AEM1") ? (num_stages * (num_stages - 1)) >> 1 :
+        (mode == "BEM1") ? (num_stages * (num_stages + 1)) >> 1 :
+        (mode == "AIM1") ? num_stages * num_stages :
+        (mode == "BIM1") ? num_stages * (num_stages + 1) : 0
     opt = LBFGSOptimizer(evaluator, evaluator', QuadraticLineSearch(),
         random_array(seed, Float64, num_variables),
         sqrt(num_variables * eps(Float64)), num_variables)
 
-    io = WRITE_FILE ? open(tempname, "w") : devnull
+    io = WRITE_FILE ? open(filename, "w") : devnull
 
     for x in opt.current_point
         fprintln(io, @sprintf("%+.16e", x))
     end
+
     fprintln(io)
+
     fprintln(io, "| ITERATION # |  RMS RESIDUAL  |  RMS GRADIENT  " *
-                 "|   RMS  COEFF   |  STEP  LENGTH  |")
+                 "| MAX ABS COEFF. |  STEP  LENGTH  |")
     fprintln(io, "|-------------|----------------|----------------" *
                  "|----------------|----------------|")
     fprint_status(io, opt; force=true)
-    while !opt.has_terminated[]
+    failed = false
+    while true
         step!(opt)
-        fprint_status(io, opt;
-            force=(opt.iteration_count[] % 1000 == 0))
+        if opt.has_terminated[]
+            break
+        end
+        if any(abs(coeff) > 1024.0 for coeff in opt.current_point)
+            failed = true
+            break
+        end
+        fprint_status(io, opt; force=(opt.iteration_count[] % 1000 == 0))
     end
     fprint_status(io, opt; force=true)
+
     fprintln(io)
+
     for x in opt.current_point
         fprintln(io, @sprintf("%+.16e", x))
     end
@@ -115,33 +151,88 @@ function search(
     coeff_score = round(Int,
         clamp(10000 - 2500 * log10(rms_coeff), 0.0, 9999.0))
 
-    filename = @sprintf("RKTK-%02d-%02d-%04d-%04d-%04d-%016X.txt", order,
-        num_stages, residual_score, gradient_score, coeff_score, seed)
+    finalname = @sprintf("RKTK-%02d-%02d-%s-%04d-%04d-%s-%016X.txt",
+        order, num_stages, mode, residual_score, gradient_score,
+        failed ? "FAIL" : @sprintf("%04d", coeff_score), seed)
     if WRITE_FILE
-        mv(tempname, filename)
+        mv(filename, finalname)
     end
-    println("Finished computing $filename.")
+    println("Finished computing $finalname.")
 end
 
 
 const SEED_COUNTER = Atomic{UInt64}(0)
 
 
-function thread_work(order::Int, num_stages::Int, max_seed::UInt64)
-    evaluator = RKOCEvaluator{Float64}(order, num_stages)
+function thread_work_ae(order::Int, num_stages::Int, max_seed::UInt64)
+    evaluator = RKOCEvaluatorAE{Float64}(order, num_stages)
     while true
         seed = atomic_add!(SEED_COUNTER, one(UInt64))
         if seed > max_seed
             break
         end
-        search(evaluator, order, num_stages, seed)
+        search(evaluator, order, num_stages, "AEM1", seed)
+    end
+end
+
+
+function thread_work_be(order::Int, num_stages::Int, max_seed::UInt64)
+    evaluator = RKOCEvaluatorBE{Float64}(order, num_stages)
+    while true
+        seed = atomic_add!(SEED_COUNTER, one(UInt64))
+        if seed > max_seed
+            break
+        end
+        search(evaluator, order, num_stages, "BEM1", seed)
+    end
+end
+
+
+function thread_work_ai(order::Int, num_stages::Int, max_seed::UInt64)
+    evaluator = RKOCEvaluatorAI{Float64}(order, num_stages)
+    while true
+        seed = atomic_add!(SEED_COUNTER, one(UInt64))
+        if seed > max_seed
+            break
+        end
+        search(evaluator, order, num_stages, "AIM1", seed)
+    end
+end
+
+
+function thread_work_bi(order::Int, num_stages::Int, max_seed::UInt64)
+    evaluator = RKOCEvaluatorBI{Float64}(order, num_stages)
+    while true
+        seed = atomic_add!(SEED_COUNTER, one(UInt64))
+        if seed > max_seed
+            break
+        end
+        search(evaluator, order, num_stages, "BIM1", seed)
+    end
+end
+
+
+function get_mode()
+    @static if REQUESTED_IMPLICIT && !REQUESTED_EXPLICIT
+        @static if REQUESTED_APPROXIMATE_B && !REQUESTED_EXACT_B
+            return "BI"
+        else
+            return "AI"
+        end
+    else
+        @static if REQUESTED_APPROXIMATE_B && !REQUESTED_EXACT_B
+            return "BE"
+        else
+            return "AE"
+        end
     end
 end
 
 
 function main(order::Int, num_stages::Int, min_seed::UInt64, max_seed::UInt64)
+    mode = get_mode()
     if WRITE_FILE
-        dirname = @sprintf("RKTK-SEARCH-%02d-%02d", order, num_stages)
+        dirname = @sprintf("RKTK-SEARCH-%02d-%02d-%s", order, num_stages, mode)
         if basename(pwd()) != dirname
             if !isdir(dirname)
                 mkdir(dirname)
@@ -150,21 +241,24 @@ function main(order::Int, num_stages::Int, min_seed::UInt64, max_seed::UInt64)
         end
     end
     SEED_COUNTER[] = min_seed
-    @threads for _ = 1:nthreads()
-        thread_work(order, num_stages, max_seed)
+    if mode == "AE"
+        @threads for _ = 1:nthreads()
+            thread_work_ae(order, num_stages, max_seed)
+        end
+    elseif mode == "BE"
+        @threads for _ = 1:nthreads()
+            thread_work_be(order, num_stages, max_seed)
+        end
+    elseif mode == "AI"
+        @threads for _ = 1:nthreads()
+            thread_work_ai(order, num_stages, max_seed)
+        end
+    else
+        @threads for _ = 1:nthreads()
+            thread_work_bi(order, num_stages, max_seed)
+        end
     end
 end
-
-
-const USAGE_STRING = """
-Usage: julia [options] $PROGRAM_FILE <order> <num_stages> [seed] [seed]
-[options] refers to Julia options, such as -O3 or --math-mode=fast.
-<order> and <num_stages> must be positive integers.
-If no seed is specified, then a search is performed for every possible seed,
-    starting from zero and counting up through every unsigned 64-bit integer.
-If one seed is specified, it is treated as an upper bound (inclusive).
-If two seeds are specified, they are treated as bounds (inclusive).
-"""
 
 
 function parse_arguments()
@@ -194,7 +288,6 @@ function parse_arguments()
             rethrow(e)
         end
     end
-
 end
 
 
